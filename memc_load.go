d2311df07ce12bd9e63e01557730a94ecaeaa406
Nikolay Kovalenko <nickolaykovalenko@improvado.io>
2018-10-27 21:09:54 +0700

    Protobuf messages Connection pool + different Memcached for devices types

--- memc_load.go
+++ memc_load.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"./appinstalled"
 	"bufio"
 	"compress/gzip"
 	"errors"
+	"flag"
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"github.com/mediocregopher/radix.v2/pool"
 	"io"
 	"log"
@@ -15,14 +18,14 @@ import (
 	"strings"
 )
 
-var files_pattern = "./sandbox/*.tsv.gz"
+const dbConnectionPoolSize = 15
 
 type AppInstalled struct {
 	devType string
 	devId   string
 	lat     float64
 	lon     float64
-	apps    []int
+	apps    []uint32
 }
 
 func readFile(path string) (<-chan string, <-chan error, error) {
@@ -75,7 +78,6 @@ func parseRecord(line string) (*AppInstalled, error) {
 		return appInstalled, errors.New(msg)
 	}
 
-	// TODO: parse it with assigning automatically
 	appInstalled.devType = lineParts[0]
 	appInstalled.devId = lineParts[1]
 	lat, err := strconv.ParseFloat(lineParts[2], 32)
@@ -91,10 +93,10 @@ func parseRecord(line string) (*AppInstalled, error) {
 
 	appsAsStrings := strings.Replace(lineParts[4], "\n", "", -1)
 	appsListAsStrings := strings.Split(appsAsStrings, ",")
-	var appsList []int
+	var appsList []uint32
 	for _, appAsString := range appsListAsStrings {
 		appId, err := strconv.Atoi(appAsString)
-		appsList = append(appsList, appId)
+		appsList = append(appsList, uint32(appId))
 		if err != nil {
 			msg := fmt.Sprintf("Can not parse string with wrong App IDs: %s", line)
 			return appInstalled, errors.New(msg)
@@ -105,28 +107,66 @@ func parseRecord(line string) (*AppInstalled, error) {
 	return appInstalled, nil
 }
 
-func insertRecord(db_pool *pool.Pool, appInstalled *AppInstalled, dryRun bool) error {
-	conn, err := db_pool.Get()
+func insertRecord(dbPools map[string]*pool.Pool, appInstalled *AppInstalled, dryRun bool) error {
+	userApp := appinstalled.UserApps{
+		Apps: appInstalled.apps,
+		Lat:  appInstalled.lat,
+		Lon:  appInstalled.lon,
+	}
+
+	messagePacked, err := proto.Marshal(&userApp)
 	if err != nil {
 		return err
 	}
-	defer db_pool.Put(conn)
+
+	key := fmt.Sprintf("%s:%s", appInstalled.devType, appInstalled.devId)
 
 	if dryRun {
-		log.Printf("%+v\n", appInstalled)
+		log.Printf("%s -> %s", key, strings.Replace(string(messagePacked), "\n", " ", -1))
 
 		// For testing DB interuction purposes
-		key := fmt.Sprintf("%s:%s", appInstalled.devType, appInstalled.devId)
-		resp := conn.Cmd("SET", key, appInstalled)
-		if resp.Err != nil {
-			log.Fatal(resp.Err)
-		}
-		respT1 := conn.Cmd("GET", key)
-		fmt.Println("Got from DB", respT1)
-		conn.Cmd("DEL", key)
+		//dbPool, ok := dbPools[appInstalled.devType]
+		//if !ok {
+		//	msg := fmt.Sprintf("Unknown device type: %s", appInstalled.devType)
+		//	return errors.New(msg)
+		//}
+		//
+		//conn, err := dbPool.Get()
+		//if err != nil {
+		//	return err
+		//}
+		//defer dbPool.Put(conn)
+		//
+		//resp := conn.Cmd("SET", key, messagePacked)
+		//if resp.Err != nil {
+		//	return resp.Err
+		//}
+		//fmt.Println("userApp is ", userApp)
+		//fmt.Println("Packed is ", messagePacked)
+		//
+		//respT1 := conn.Cmd("GET", key)
+		//fmt.Println("Got from DB", respT1)
+		//conn.Cmd("DEL", key)
+		//userAppUnpacked := &appinstalled.UserApps{}
+		//respT1Bytes, _ := respT1.Bytes()
+		//if err := proto.Unmarshal(respT1Bytes, userAppUnpacked); err != nil {
+		//	return err
+		//}
+		//fmt.Println("userAppUnpacked is ", userAppUnpacked)
 	} else {
-		key := fmt.Sprintf("%s:%s", appInstalled.devType, appInstalled.devId)
-		resp := conn.Cmd("SET", key, appInstalled)
+		dbPool, ok := dbPools[appInstalled.devType]
+		if !ok {
+			msg := fmt.Sprintf("Unknown device type: %s", appInstalled.devType)
+			return errors.New(msg)
+		}
+
+		conn, err := dbPool.Get()
+		if err != nil {
+			return err
+		}
+		defer dbPool.Put(conn)
+
+		resp := conn.Cmd("SET", key, messagePacked)
 		if resp.Err != nil {
 			return resp.Err
 		}
@@ -135,60 +175,93 @@ func insertRecord(db_pool *pool.Pool, appInstalled *AppInstalled, dryRun bool) e
 	return nil
 }
 
-func handleFile(dbPool *pool.Pool, filesNames []string, dryRun bool) {
-	for _, fileName := range filesNames {
-		lines, readingErrs, err := readFile(fileName)
-		if err != nil {
-			log.Fatal(err)
-			continue
+func handleFile(dbPools map[string]*pool.Pool, fileName string, dryRun bool) error {
+	lines, readingErrs, err := readFile(fileName)
+	if err != nil {
+		return err
+	}
+	for {
+		select {
+		case line, ok := <-lines:
+			if ok {
+				appInstalled, err := parseRecord(line)
+				if err != nil {
+					log.Printf("Can not convert line into an inner entity %s: %s", fileName, err)
+				}
+
+				err = insertRecord(dbPools, appInstalled, dryRun)
+				if err != nil {
+					log.Printf("Can not save inner inner entity into DB: %s", err)
+				}
+			} else {
+				lines = nil
+			}
+		case err, ok := <-readingErrs:
+			if ok {
+				log.Printf("Error reading file %s: %s", fileName, err)
+			} else {
+				readingErrs = nil
+			}
 		}
-		for {
-			select {
-			case line, ok := <-lines:
-				if ok {
-					appInstalled, err := parseRecord(line)
-					if err != nil {
-						log.Printf("Can not convert line into an inner entity %s: %s", fileName, err)
-					}
 
-					err = insertRecord(dbPool, appInstalled, dryRun)
-					if err != nil {
-						log.Printf("Can not save inner inner entity into DB: %s", err)
-					}
-				} else {
-					lines = nil
-				}
-			case err, ok := <-readingErrs:
-				if ok {
-					log.Printf("Error reading file %s: %s", fileName, err)
-				} else {
-					readingErrs = nil
-				}
-			}
+		if lines == nil && readingErrs == nil {
+			break
+		}
+	}
 
-			if lines == nil && readingErrs == nil {
-				break
-			}
+	return nil
+}
+
+func startLoading(filesPattern string, deviceIdVsMemcHost map[string]*string, isRunDry bool) {
+	deviceIdVsMemcPool := make(map[string]*pool.Pool)
+	for deviceId, memcHost := range deviceIdVsMemcHost {
+		dbPool, err := pool.New("tcp", *memcHost, dbConnectionPoolSize)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		deviceIdVsMemcPool[deviceId] = dbPool
+	}
+
+	filesMatches, err := filepath.Glob(filesPattern)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fileName := range filesMatches {
+		err := handleFile(deviceIdVsMemcPool, fileName, isRunDry)
+		if err != nil {
+			log.Printf("Can not parse file %s: %s", fileName, err)
 		}
 	}
 }
 
 func main() {
-	filesMatches, err := filepath.Glob(files_pattern)
-	if err != nil {
-		log.Fatal(err)
+	filesPattern := flag.String("pattern", "./sandbox/*.tsv.gz", "File pattern, a string")
+	isRunDry := flag.Bool("dry", true, "Run without saving to DB, a bool")
+
+	if logFilePath := flag.String("log", "", "Path to a log file, a string"); *logFilePath != "" {
+		f, err := os.OpenFile(*logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
+
+		log.SetOutput(f)
 	}
 
-	dbPool, err := pool.New("tcp", "localhost:6379", 10)
-	if err != nil {
-		log.Panic(err)
+	deviceIdVsMemcHost := map[string]*string{
+		"idfa": flag.String("idfa", "127.0.0.1:33013", "IDFA device Memcached host, a string"),
+		"gaid": flag.String("gaid", "127.0.0.1:33014", "GAID device Memcached host, a string"),
+		"adid": flag.String("adid", "127.0.0.1:33015", "ADID device Memcached host, a string"),
+		"dvid": flag.String("dvid", "127.0.0.1:33016", "DVID device Memcached host, a string"),
 	}
 
-	dryRun := true
-	handleFile(dbPool, filesMatches, dryRun)
+	startLoading(*filesPattern, deviceIdVsMemcHost, *isRunDry)
 }
 
-// Todo: Pass options to the script
+// Todo: A few Memcacheds + Docker
 // Todo: Count errors while files processing
+
 // Todo: Implement reconnect / timeouts for Redis
 // Todo: Apply concurrency
